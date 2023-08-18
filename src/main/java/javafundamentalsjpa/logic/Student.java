ee7088066753e1fd0e02a38f9cd6ea1015ac8f44
Jaime Pineda <jpin730@pm.me>
2023-08-18 11:25:19 -0600

    feat: :sparkles: add student entity

---
+++ src/main/java/javafundamentalsjpa/logic/Student.java
@@ -0,0 +1,66 @@
+package javafundamentalsjpa.logic;
+
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Basic
+    private String firstName;
+    private String lastName;
+    @Temporal(TemporalType.DATE)
+    private Date birthdate;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String firstName, String lastName, Date birthdate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+}
